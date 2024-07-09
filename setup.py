863c09f44d0598de701f30285f1784bdfa6f4b7a
kshivashankarai <k.shivashankar.ai@gmail.com>
2024-07-10 00:04:27 +0530

    Add the setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='chat_with_postgres_db',
+    version='0.0.1',
+    author='shiva shankar',
+    author_email='ji.shiva1998@Gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","langchain-groq","langchain_community"],
+    packages=find_packages()
+)
